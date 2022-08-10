c2fe13c66527b1c21825e274e943db1df78b2588
Nick Pachulski <nick@pachulski.me>
2022-08-10 06:46:12 -0400

    Add eslint and set the base directory to `src`

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-native", "@typescript-eslint", "react-hooks"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "tsconfig.json",
+      },
+    },
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+      },
+    },
+  },
+  env: {
+    "react-native/react-native": true,
+  },
+  rules: {
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-single-element-style-arrays": 2,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "@typescript-eslint/switch-exhaustiveness-check": "error",
+    "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/no-empty-function": 0,
+  },
+}
