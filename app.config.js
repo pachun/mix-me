c7f8e081ef23c519ead553170aa7a7d9df725d28
Nick Pachulski <nick@pachulski.me>
2022-08-12 13:54:05 -0400

    Automatically deploy to testers OTA w/ GH Actions

---
+++ app.config.js
@@ -0,0 +1,59 @@
+const DEVELOPMENT_ENV = process.env.APP_VARIANT === "development"
+const MAIN_ENV = process.env.APP_VARIANT === "main"
+const PRODUCTION_ENV = process.env.APP_VARIANT === "production"
+
+const name = () => {
+  if (DEVELOPMENT_ENV) {
+    return "Mix Me (dev)"
+  } else if (MAIN_ENV) {
+    return "Mix Me (main)"
+  } else {
+    return "Mix Me"
+  }
+}
+
+const bundleIdentifier = () => {
+  if (DEVELOPMENT_ENV) {
+    return "com.pachun.mixme.development"
+  } else if (MAIN_ENV) {
+    return "com.pachun.mixme.main"
+  } else {
+    return "com.pachun.mixme"
+  }
+}
+
+export default {
+  name: name(),
+  slug: "mix-me",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 5000,
+    url: "https://u.expo.dev/6b860cd9-090b-4bb2-a1ac-b18bb3bea241",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: bundleIdentifier(),
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+    package: bundleIdentifier(),
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  runtimeVersion: {
+    policy: "sdkVersion",
+  },
+}
